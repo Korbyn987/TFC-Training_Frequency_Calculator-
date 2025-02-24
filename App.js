d6520414760079fd8b1e44e4c6487d8728810b7d
Korbyn987 <korbynirvin@gmail.com>
2025-02-23 22:24:08 -0600

    making tfc react native app

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Provider } from "react-redux";
+import store from "./src/redux/store"; // Ensure this path is correct
+import HomeScreen from "./src/screens/HomeScreen"; // Create this screen
+import DetailsScreen from "./src/screens/DetailsScreen"; // Create this screen
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+// this is meant to serve as basic screens for navigation
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+export default DetailsScreen;
