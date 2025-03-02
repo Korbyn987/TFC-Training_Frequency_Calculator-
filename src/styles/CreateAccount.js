970bb7e35a7edd34c30dc5b7ec7b5372a7404cd4
Korbyn987 <korbynirvin@gmail.com>
2025-03-02 15:27:05 -0600

    created basic classes for the login and create account portion of the application, will work on the css once the doc is sent with the BR's

--- src/styles/CreateAccount.js
+++ src/styles/CreateAccount.js
@@ -3,5 +3,9 @@ import { Stylesheet, Dimensions } from "react-native";
 const { width, height } = Dimensions.get("window");
 
 export const createAccountStyles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
--- src/styles/Login.js
+++ src/styles/Login.js
@@ -1,4 +1,12 @@
 import React from "react";
 import { StyleSheet, Dimensions } from "react-native";
 
-export default StyleSheet.create({});
+const { width, height } = Dimensions.get("window");
+
+export const loginAccountStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
