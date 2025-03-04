cd1913bc96eaf3e8d6d9e04a381bf0c5b5102c0d
Adam Hendler <adamhendlermail@gmail.com>
2025-03-04 12:35:09 -0600

    I changed the code to allow users to add their gender

--- Backend/user_management.py
+++ Backend/user_management.py
@@ -23,6 +23,7 @@ def register_user():
     # Get user info
     name = input("Enter your full name: ")
     age = input("Enter your age: ")
+    gender = input("Enter your gender (Male/Female/Other): ")
     weight = input("Enter your weight (in lbs): ")
     height = input("Enter your height (e.g., 5'11\"): ")
     
@@ -43,9 +44,9 @@ def register_user():
             
             # Insert into user info table
             cursor.execute("""
-                INSERT INTO TFC_user_info_table (id, name, age, weight, height)
-                VALUES (?, ?, ?, ?, ?)
-            """, (next_id, name, age, weight, height))
+                INSERT INTO TFC_user_info_table (id, name, age, gender, weight, height)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, (next_id, name, age, gender, weight, height))
             
             conn.commit()
             print("\nUser registered successfully!")
@@ -66,7 +67,7 @@ def login_user():
         try:
             cursor = conn.cursor()
             cursor.execute("""
-                SELECT u.name, u.age, u.weight, u.height
+                SELECT u.name, u.age, u.gender, u.weight, u.height
                 FROM TFC_login_table l
                 JOIN TFC_user_info_table u ON l.id = u.id
                 WHERE l.username = ? AND l.password = ?
@@ -78,8 +79,9 @@ def login_user():
                 print(f"Welcome back, {user_data[0]}!")
                 print(f"Your profile:")
                 print(f"Age: {user_data[1]}")
-                print(f"Weight: {user_data[2]} lbs")
-                print(f"Height: {user_data[3]}")
+                print(f"Gender: {user_data[2]}")
+                print(f"Weight: {user_data[3]} lbs")
+                print(f"Height: {user_data[4]}")
             else:
                 print("\nInvalid username or password.")
                 
