449189b4215958e12227fa27c62d825323abd6e1
Adam Hendler <adamhendlermail@gmail.com>
2025-04-16 20:33:23 -0500

    Fuck if I know Im just glad it all works again

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const webpack = require('webpack');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
@@ -20,6 +21,23 @@ module.exports = async function (env, argv) {
     '.ts',
     '.tsx'
   ];
+
+  // Add Node.js polyfills for newer Node versions
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "buffer": require.resolve("buffer/"),
+    "process": require.resolve("process/browser"),
+  };
+
+  // Add plugins for polyfills
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    })
+  );
 
   return config;
 };
